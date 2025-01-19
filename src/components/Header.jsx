f5ef6ffa48a6bfe225ebf94ca292ef35b71ce9fe
gkirber <george.kirber@gmail.com>
2025-01-19 23:35:05 +0100

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.mwimmerdesign.de/wp-content/uploads/2023/01/wort-marke.svg"
+                 alt="logo"/>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <div className="content">
+                <img className="img_cont"
+                     src="https://st.depositphotos.com/1000124/1476/i/950/depositphotos_14766203-stock-photo-wood-textured.jpg"
+                     alt="main content"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
